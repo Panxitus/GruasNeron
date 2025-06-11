c2e4cc460be518ae0a9359b97f3b9f753e0ddd56
Panxitus <gonzalezuribe.francisco@gmail.com>
2025-06-10 23:25:16 -0400

    integracion de logo y componentes atoms

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Homepage from "./components/pages/homepage"
-
 function App() {
   
   return (
---
+++ src/components/atoms/logo.jsx
@@ -0,0 +1,11 @@
+import logo from '../../assets/images/logo.jpeg'
+
+const logo = () => {
+  return (
+    <div>
+        <img src={logo} alt="logo de empresa"/>
+    </div>
+  )
+}
+
+export default logo
